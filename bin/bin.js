01f4be86d69fa0d29d11b4df0d1ae5546e3b5f80
browniu <niuniusoga@qq.com>
2019-08-07 11:41:11 +0800

    +

--- bin/bin.js
+++ bin/bin.js
@@ -1,32 +1,82 @@
 #! /usr/bin/env node
+// 依赖组件
 const processC = require('child_process');
 const request = require('request')
-
+const urlencode = require('urlencode')
+const cheerio = require('cheerio')
+const isChinese = require('is-chinese')
+const chalk = require('chalk');
+const fs = require('fs')
 // 指令处理
 const word = process.argv.slice(2).join(' ')
 if (!word) {
     console.log('使用方式：c <word>')
     process.exit()
 }
+const isCh = isChinese(word)
 
 //常量
 const serves = {
     dict: 'http://dict.youdao.com/w/',
+    dictCh: 'https://dict.youdao.com/w/eng/',
     book: 'https://test-1257187612.cos.ap-shanghai.myqcloud.com/data.json',
     push: 'https://api.day.app/NmAByzvdmM8EfTtNsYMGEo/'
 }
+const dataPath = './data.json'
 
+// 数据解析
 async function handleCheck() {
-    const result = await requestTarget(serves.dict)
-    console.log(result)
+
+    const body = await requestTarget((isCh ? serves.dictCh : serves.dict) + urlencode(word))
+    const $ = cheerio.load(body)
+    let result = ''
+
+    if (isCh) {
+        $('div.trans-container > ul').find('p.wordGroup').each(function () {
+            result += $(this).children('span.contentTitle').text().replace(/\s+/g, '').substr(0, 50)
+        })
+        result = result.replace(/;/g, ' ')
+        if (result) console.log(chalk.green(result))
+    } else {
+        $('div#phrsListTab > div.trans-container > ul').find('li').each(function () {
+            result += $(this).text().replace(/\s+/g, '').substr(0, 30) + ' '
+        })
+        result = result.replace(/；/g, ';')
+        if (result) console.log(chalk.blue(result))
+    }
+    if (!result) console.log(chalk.red('查询无果'))
+    else record({word: word, result: result, date: new Date().getTime()}, dataPath)
 }
 
+// 发送请求
 function requestTarget(target) {
     return new Promise(resolve => {
         request(target, (error, response) => {
             resolve(response.body)
         })
     })
+}
+
+// 记录
+async function record(item, path) {
+    let list = await recordRead(path)
+    list.push(item)
+    fs.writeFile(path, JSON.stringify(list), (err) => {if (err) throw (err)})
+}
+
+// 读取记录
+function recordRead(path) {
+    return new Promise(resolve => {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) throw (err)
+            resolve(JSON.parse(data))
+        })
+    })
+}
+
+// 上传记录
+function recordCloud(record) {
+
 }
 
 handleCheck()
